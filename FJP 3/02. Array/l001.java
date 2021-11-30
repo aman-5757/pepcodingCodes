63222ec6f89e6b296e170f36fe515d31d809318d
Aman Srivastava <aman4863@gmail.com>
2021-11-30 22:59:58 +0530

    fjp 2

--- FJP 3/02. Array/l001.java
+++ FJP 3/02. Array/l001.java
@@ -3,6 +3,54 @@ public class l001{
     // infi - 1e8
     // math.min - max -- if else
 
+    //span
+    // {
+    //     int max = -(int)1e9;
+    //     int min = (int)1e9;
+
+    //     for(int i = 0; i<n; i++){
+    //         if(arr[i] > max){
+    //             max = arr[i];
+    //         }
+    //         if(arr[i] < min){
+    //             min = arr[i];
+    //         }
+    //     }
+
+    //     System.out.println(max-min);
+    // }
+
+
+    //bar chart
+
+    public static int maximum(int [] arr){
+        int max = -(int)1e9;
+        for(int i = 0; i<arr.length; i++){
+            if(max< arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void barChart(int [] arr){
+        int height = maximum(arr);
+        
+        for(int h = height ; h>=1 ; h--){
+            for(int i = 0; i<arr.length; i++){
+                if(arr[i] >= h){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+
 
     public static void main(String [] args){
         Scanner scn = new Scanner(System.in);
@@ -11,19 +59,8 @@ public class l001{
         for(int i = 0; i<n; i++){
             arr[i] = scn.nextInt();
         } 
+        barChart(arr);
 
-        int max = -(int)1e9;
-        int min = (int)1e9;
-
-        for(int i = 0; i<n; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
-            if(arr[i] < min){
-                min = arr[i];
-            }
-        }
-
-        System.out.println(max-min);
+        
     }
 }
