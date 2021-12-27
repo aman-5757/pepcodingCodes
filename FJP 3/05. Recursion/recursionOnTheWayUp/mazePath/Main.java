d999bef06576f34e29c686e3d1b98780a78def02
Aman Srivastava <aman4863@gmail.com>
2021-12-27 23:56:31 +0530

    added

--- FJP 3/05. Recursion/recursionOnTheWayUp/mazePath/Main.java
+++ FJP 3/05. Recursion/recursionOnTheWayUp/mazePath/Main.java
@@ -16,6 +16,16 @@ import java.io.*;
 	    // dr - destination row
 	    // dc - destination column
 	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	       // if(sr == dr && sc == dc){
+	       //     System.out.println(psf);
+	       //     return;
+	       // }
+	       // if(sr > dr || sc > dc)
+	       //     return;
+	       // printMazePaths(sr, sc+1, dr, dc, psf + "h");
+	       // printMazePaths(sr + 1, sc, dr, dc, psf+"v");
+	        
+	        
 	        if(sr == dr && sc == dc){
 	            System.out.println(psf);
 	            return;
