373c2e369dce6c89458688b9a5f859e86c771e9f
Aman Srivastava <aman4863@gmail.com>
2022-01-03 23:50:52 +0530

    added

--- FJP 3/05. Recursion/backtracking/nQueen/Main.java
+++ FJP 3/05. Recursion/backtracking/nQueen/Main.java
@@ -4,12 +4,37 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [][] chess = new int[n][n];
+        printNQueens(chess, "", 0);
         
     }
     public static boolean isSafe(int r, int c, int [][] chess){
+        int n = chess.length;
+        //1. 
+        for(int i = r-1, j = c + 1; i >= 0 && j < n; i--, j++ )
+        {
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
         
+        //2.
+        for(int i = r-1, j = c; i >= 0 ; i--){
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
         
-        //logic
+        //3. 
+        for(int i = r-1, j = c-1; i >= 0 && j >= 0; i--, j--){
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+        
+        return true;
     }
     public static void printNQueens(int[][] chess, String asf, int row) {
         if(row == chess.length){
@@ -18,15 +43,11 @@ public class Main {
         }
         
         for(int col = 0; col<chess.length; col++){
-            if(isSafe()){
+            if(isSafe(row,col,chess)){
                 chess[row][col] = 1;
                 printNQueens(chess, asf + row + "-" + col + ", ", row + 1);
                 chess[row][col] = 0;
             }
         }
-        
-        
-        
-        
     }
 }
