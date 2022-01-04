c128e425cf76e2ea4c0f32c7cc1ddd64813208ef
Aman Srivastava <aman4863@gmail.com>
2022-01-04 22:40:19 +0530

    added

---
+++ DSA December/01. patterns/p11/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int val = 1;
+        for(int i = 1; i<= n; i++ ){
+            for(int j = 1; j<=i; j++){
+                System.out.print(val+"\t");
+                val++;
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
