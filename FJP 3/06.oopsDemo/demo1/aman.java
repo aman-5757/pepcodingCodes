4deaafbee1192ff7c5cf5a483eb1d441afa5e8ce
Aman Srivastava <aman4863@gmail.com>
2022-01-05 23:55:51 +0530

    added

---
+++ FJP 3/06.oopsDemo/demo1/aman.java
@@ -0,0 +1,12 @@
+
+
+public class aman{
+    int x = 10;
+    void fun(){
+        System.out.println();
+    }
+
+    public static void main(String [] args){
+        System.out.println("pepcoding");
+    }
+}
