3a49f00fbb1204f9599f02c55a4f4007e4d6d5ce
Aman Srivastava <aman4863@gmail.com>
2022-01-06 22:10:05 +0530

    added

--- DSA December/01. patterns/p16/Main.java
+++ DSA December/01. patterns/p16/Main.java
@@ -9,22 +9,28 @@ public static void main(String[] args) {
      
      int star = 1;
      int space = 2 * n - 3;
-     int val = 1;
      
      for(int i = 1; i<=n; i++){
+         
+        //for every row
+        int val = 1;
          for(int cst = 1; cst <= star; cst++ ){
              System.out.print(val+"\t");
+             val++;
          }
          for(int csp = 1; csp <= space; csp++ ){
              System.out.print("\t");
          }
+         
          if(i == n){
+             val--;
              star--;
          }
+         
          for(int cst = 1; cst <= star; cst++ ){
+             val--;
              System.out.print(val+"\t");
          }
-         
          System.out.println();
          
          //update
