d5d721421b294618b624c725223d98fa4fba92c3
Aman Srivastava <aman4863@gmail.com>
2022-01-06 22:53:32 +0530

    added

---
+++ DSA December/01. patterns/p18/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+     int n = scn.nextInt();
+     int star = n;
+     int space = 0;
+     
+     
+     for(int i = 1; i<= n; i++){
+         for(int csp = 1; csp <= space; csp++){
+             System.out.print("\t");
+         }
+         for(int cst = 1; cst <= star; cst++){
+             System.out.print("*\t");
+         }
+         //update
+         if(i <= n/2){
+            star -= 2;
+            space += 1;
+         }
+         else{
+            star += 2;
+            space -= 1;             //spcae--  or spcae = space - 1;
+         }
+         System.out.println();
+         
+         
+     }
+
+ }
+}
