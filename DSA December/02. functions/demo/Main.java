42e1c74e094a9a0a52ea6a7c104948b8d175b7e9
Aman Srivastava <aman4863@gmail.com>
2022-01-08 20:53:41 +0530

    added

---
+++ DSA December/02. functions/demo/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+
+    // public static int sum(int x, int y){
+    //     return x+y;
+    // }
+
+
+    public static void function1(){
+        System.out.println("Pepcoding");
+    }
+
+    public static void function2(int age){
+        System.out.println("My age is : " + age);
+    }
+
+    //return type yes & parameter no
+    public static int function3(){
+        return 10 + 50;
+    }
+
+    //return type yes & parameter yes
+    public static int function4(int x, int y, int z){
+        return x+y+z;
+    }
+
+
+    public static void main(String [] args){
+        // int x = 10;
+        // int y = 20;
+        // int answer = sum(x,y);
+        // System.out.println(answer);
+        // function1();
+        // function2(100);
+        // int ans = function3();
+        int ans = function4(10,20,50);
+        System.out.println(ans);
+
+
+    }
+}
