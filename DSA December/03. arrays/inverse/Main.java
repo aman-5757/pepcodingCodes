ecccb81c1059e9406b0fb375e41f8ed316e2e3dc
Aman Srivastava <aman4863@gmail.com>
2022-01-11 22:22:37 +0530

    added

---
+++ DSA December/03. arrays/inverse/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static int[] inverse(int[] a){
+    int [] nArr = new int[a.length];
+    for(int i = 0; i<a.length; i++){
+        int val = a[i];
+        nArr[val] = i;
+    }
+    return nArr;
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] inv = inverse(a);
+    display(inv);
+ }
+
+}
