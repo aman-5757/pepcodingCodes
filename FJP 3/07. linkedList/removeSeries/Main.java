b55133093a48b1fa3db4c7c4b55c2257cca915b7
Aman Srivastava <aman4863@gmail.com>
2022-01-13 00:05:44 +0530

    added

--- FJP 3/07. linkedList/removeSeries/Main.java
+++ FJP 3/07. linkedList/removeSeries/Main.java
@@ -2,197 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static class Node {
-    int data;
-    Node next;
-  }
 
-  public static class LinkedList {
-    Node head;
-    Node tail;
-    int size;
+  public static class LLToQueueAdapter {
+    LinkedList<Integer> list;
 
-    void addLast(int val) {
-      Node temp = new Node();
-      temp.data = val;
-      temp.next = null;
-
-      if (size == 0) {
-        head = tail = temp;
-      } else {
-        tail.next = temp;
-        tail = temp;
-      }
-
-      size++;
+    public LLToQueueAdapter() {
+      list = new LinkedList<>();
     }
 
-    public int size() {
-      return size;
+    int size() {
+      return list.size();
     }
 
-    public void display() {
-      for (Node temp = head; temp != null; temp = temp.next) {
-        System.out.print(temp.data + " ");
-      }
-      System.out.println();
+    void add(int val) {
+      list.addLast(val);
     }
 
-    public void removeFirst() {
-      if (size == 0) {
-        System.out.println("List is empty");
-      } else if (size == 1) {
-        head = tail = null;
-        size = 0;
-      } else {
-        head = head.next;
-        size--;
-      }
-    }
-
-    public int getFirst() {
-      if (size == 0) {
-        System.out.println("List is empty");
-        return -1;
-      } else {
-        return head.data;
-      }
-    }
-
-    public int getLast() {
-      if (size == 0) {
-        System.out.println("List is empty");
-        return -1;
-      } else {
-        return tail.data;
-      }
-    }
-
-    public int getAt(int idx) {
-      if (size == 0) {
-        System.out.println("List is empty");
-        return -1;
-      } else if (idx < 0 || idx >= size) {
-        System.out.println("Invalid arguments");
-        return -1;
-      } else {
-        Node temp = head;
-        for (int i = 0; i < idx; i++) {
-          temp = temp.next;
+    int remove() {
+        if(list.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
         }
-        return temp.data;
-      }
+        else{
+           return list.removeFirst(); 
+        }
+
     }
 
-    public void addFirst(int val) {
-      Node temp = new Node();
-      temp.data = val;
-      temp.next = head;
-      head = temp;
+    int peek() {
+        if(list.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        else{
+            return list.getFirst();
+        }
       
-      if(size == 0){
-        tail = temp;
-      }
-
-      size++;
-    }
-
-    public void addAt(int idx, int val){
-      if(idx < 0 || idx > size){
-        System.out.println("Invalid arguments");
-      } else if(idx == 0){
-        addFirst(val);
-      } else if(idx == size){
-        addLast(val);
-      } else {
-        Node node = new Node();
-        node.data = val;
-
-        Node temp = head;
-        for(int i = 0; i < idx - 1; i++){
-          temp = temp.next;
-        }
-        node.next = temp.next;
-
-        temp.next = node;
-        size++;
-      }
-    }
-    
-    private Node getNodeAt(int idx) {
-      if (size == 0) {
-        System.out.println("List is empty");
-        return null;
-      } else if (idx < 0 || idx >= size) {
-        System.out.println("Invalid arguments");
-        return null;
-      } else {
-        Node temp = head;
-        for (int i = 0; i < idx; i++) {
-          temp = temp.next;
-        }
-        return temp;
-      }
-    }
-    
-    public void removeLast(){
-      if(size == 0){
-        System.out.println("List is empty");      
-      }
-      else if(size == 1){
-          head = null;
-          tail = null;
-      }
-      else{
-          Node secLastNode = getNodeAt(size-2);
-          secLastNode.next = null;
-          tail = secLastNode;
-      }
-      size--;
     }
   }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    LinkedList list = new LinkedList();
+    LLToQueueAdapter qu = new LLToQueueAdapter();
 
     String str = br.readLine();
     while (str.equals("quit") == false) {
-      if (str.startsWith("addLast")) {
+      if (str.startsWith("add")) {
         int val = Integer.parseInt(str.split(" ")[1]);
-        list.addLast(val);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
       } else if (str.startsWith("size")) {
-        System.out.println(list.size());
-      } else if (str.startsWith("display")) {
-        list.display();
-      } else if (str.startsWith("removeFirst")) {
-        list.removeFirst();
-      } else if (str.startsWith("getFirst")) {
-        int val = list.getFirst();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getLast")) {
-        int val = list.getLast();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = list.getAt(idx);
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("addFirst")) {
-        int val = Integer.parseInt(str.split(" ")[1]);
-        list.addFirst(val);
-      } else if (str.startsWith("addAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = Integer.parseInt(str.split(" ")[2]);
-        list.addAt(idx, val);
-      } else if (str.startsWith("removeLast")) {
-        list.removeLast();
-      } 
+        System.out.println(qu.size());
+      }
       str = br.readLine();
     }
   }
