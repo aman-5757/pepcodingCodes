d6dee3d2fd7727b5c6e6c8c57057796fac47f584
Aman Srivastava <aman4863@gmail.com>
2022-01-17 23:07:31 +0530

    added

--- FJP 3/07. linkedList/questions/Main.java
+++ FJP 3/07. linkedList/questions/Main.java
@@ -300,6 +300,46 @@ public class Main {
       this.tail = nl.tail;
       this.size = nl.size;
     }
+
+
+    //7. Odd Even
+    public void oddEven(){
+      LinkedList odd = new LinkedList();
+      LinkedList even = new LinkedList();
+      
+      while(this.size > 0){
+          int val = this.getFirst();
+          this.removeFirst();
+          
+          if(val % 2 == 0){
+              //even
+              even.addLast(val);
+          }
+          else{
+              //odd
+              odd.addLast(val);
+          }
+          
+      }
+       if(odd.size > 0 && even.size > 0 ){
+            odd.tail.next = even.head;
+            this.head = odd.head;
+            this.tail = even.tail;
+            this.size = odd.size + even.size;
+        }
+        else if(odd.size > 0){
+            this.head = odd.head;
+            this.tail = odd.tail;
+            this.size = odd.size;  
+        }
+        else if(even.size > 0){
+            this.head = even.head;
+            this.tail = even.tail;
+            this.size = even.size;    
+        }
+        
+        
+    }
   }
 
   public static void main(String[] args) throws Exception {
