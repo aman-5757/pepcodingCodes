9a1ba3081b52f5501c2652afd5378857b6dbfb4f
Aman Srivastava <aman4863@gmail.com>
2022-01-18 23:15:12 +0530

    added

--- DSA December/04. 2dArray/ringRotate/Main.java
+++ DSA December/04. 2dArray/ringRotate/Main.java
@@ -5,16 +5,107 @@ public class Main {
     public static Scanner scn = new Scanner(System.in);
     
     
+    public static void input(int [][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+    }
+    
     public static int [] from2Dto1D(int [][] arr, int s){
+        int n = arr.length;
+        int m = arr[0].length;
         
+        int minr = s-1;
+        int minc = s-1;
+        int maxr = n-s;
+        int maxc = m-s;
+        
+        int size = 2*(maxr - minr + maxc - minc);
+        
+        int idx = 0;        //index for oneD traversal
+        int [] oneD = new int[size];
+        
+        for(int i = minr ; i<= maxr; i++){
+            oneD[idx] = arr[i][minc];
+            idx++;
+        }
+        minc++;
+        for(int j = minc ; j <= maxc; j++){
+            oneD[idx] = arr[maxr][j];
+            idx++;
+        }
+        maxr--;
+        for(int i = maxr; i >= minr; i--){
+            oneD[idx++] = arr[i][maxc];
+        }
+        maxc--;
+        for(int j = maxc; j >= minc; j--){
+            oneD[idx++] = arr[minr][j];
+        }
+        minr++;
+        
+        return oneD;
     }
     
-    public static void rotate(int [] oneD, int r){
-        
+    public static void swap(int [] a, int i, int j){
+       int temp = a[i];
+       a[i] = a[j];
+       a[j] =  temp;
+       
+  }
+    public static void reverse(int [] a, int i, int j){
+       while(i <= j){
+        swap(a, i,j);
+        i++;
+        j--;
+    } 
     }
+    public static void rotate(int[] a, int k){
+      int n = a.length;
+      k %= n;
+      if (k < 0)
+        k += n;
     
+    reverse(a, 0 , n-k-1);
+    reverse(a, n-k , n-1);
+    reverse(a, 0 , n-1);
+    
+  }
+
     
     public static void from1Dto2D(int [] oneD, int [][] arr, int s){
+        int n = arr.length;
+        int m = arr[0].length;
+        
+        int minr = s-1;
+        int minc = s-1;
+        int maxr = n-s;
+        int maxc = m-s;
+        
+        
+        
+        int idx = 0;        //index for oneD traversal
+        
+        for(int i = minr ; i<= maxr; i++){
+             arr[i][minc] = oneD[idx];
+            idx++;
+        }
+        minc++;
+        for(int j = minc ; j <= maxc; j++){
+            arr[maxr][j] = oneD[idx] ;
+            idx++;
+        }
+        maxr--;
+        for(int i = maxr; i >= minr; i--){
+             arr[i][maxc] = oneD[idx++] ;
+        }
+        maxc--;
+        for(int j = maxc; j >= minc; j--){
+            arr[minr][j] = oneD[idx++] ;
+        }
+        minr++;
         
     }
     
