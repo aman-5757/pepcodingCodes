d8f23b7961cc231ee1857c0d3c55f1d8e97a6056
Aman Srivastava <aman4863@gmail.com>
2022-01-25 21:58:18 +0530

    added

---
+++ DSA December/06. Recursion/intro/printIncreasing/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n == 0){
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
+
+}
