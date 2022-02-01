376d97fcf3e01426aecc163e92033695bf3bee95
Aman Srivastava <aman4863@gmail.com>
2022-02-01 22:47:03 +0530

    added

--- DSA December/06. Recursion/recursionWithAL/getKPC/Main.java
+++ DSA December/06. Recursion/recursionWithAL/getKPC/Main.java
@@ -13,10 +13,32 @@ public class Main {
     }
 
     public static ArrayList<String> getKPC(String str) {
-        if(){
-            
+        if(str.length() == 0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
         }
-        ArrayList<String> recAns = getKPC();
+        
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        
+        ArrayList<String> recAns = getKPC(ros);
+        ArrayList<String> myAns = new ArrayList<>();
+        
+        
+        int idx = ch - '0';
+        
+        String currCode = codes[idx];   //vwx
+        
+        for(int i = 0; i<currCode.length(); i++){
+            char newChar = currCode.charAt(i);
+            for(String x : recAns){
+                myAns.add(newChar + x);
+            }
+        }
+        
+        return myAns;
+        
     }
 
 }
