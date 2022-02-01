6d4742146df85a6d93ea21877475f60dd2b91171
Aman Srivastava <aman4863@gmail.com>
2022-02-01 22:21:21 +0530

    added

---
+++ DSA December/06. Recursion/recursionWithAL/getKPC/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static String [] codes = {".;","abc", "def", "ghi", "jkl", "mno", "pqrs", "tu","vwx","yz"};
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.next();
+        ArrayList<String> ans = getKPC(s);
+        System.out.println(ans);
+    }
+
+    public static ArrayList<String> getKPC(String str) {
+        if(){
+            
+        }
+        ArrayList<String> recAns = getKPC();
+    }
+
+}
