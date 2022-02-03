af28ad8d3ee1607eb85ed01ac3b44708ac2e8075
Aman Srivastava <aman4863@gmail.com>
2022-02-03 22:14:46 +0530

    added

---
+++ DSA December/06. Recursion/recursionOntheWayUp/printMazePath/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+	import java.util.*;
+
+	public class Main {
+
+	    public static void main(String[] args) throws Exception {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int m = scn.nextInt();
+            printMazePaths(0,0,n-1,m-1, "");
+	    }
+
+	    // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        if(sr == dr && sc == dc){
+	            System.out.println(psf);
+	            return;
+	        }
+	        if(sc + 1  <= dc ){
+	            printMazePaths(sr, sc+1, dr, dc, psf + "h");
+	        }
+	        if(sr + 1  <= dr ){
+	            printMazePaths(sr+1, sc, dr, dc, psf + "v");
+	        }
+	    }
+
+	}
