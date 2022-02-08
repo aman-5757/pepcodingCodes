2fb99407690970241bf13938f99389f52ba0fac6
Aman Srivastava <aman4863@gmail.com>
2022-02-08 22:59:37 +0530

    n queen

---
+++ DSA December/06. Recursion/backtracking/nQueen/Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [][] chess = new int[n][n];
+        printNQueens(chess, "", 0);
+    }
+
+    public static boolean isSafeToPlace(int [][] chess, int r, int c){
+        //1.
+        for(int i = r-1, j = c-1; i >= 0 && j >= 0 ; i--, j--){
+            if(chess[i][j] == 1)
+                return false;
+        }
+        // 2.
+        for(int i = r-1, j = c; i >= 0 ; i--){
+            if(chess[i][j] == 1)
+                return false;
+        }
+        //3.
+        for(int i = r-1, j = c+1; i >= 0 && j < chess.length ; i--, j++){
+            if(chess[i][j] == 1)
+                return false;
+        }
+        return true;
+    }
+
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+        if(row == chess.length ){
+            System.out.println(qsf + ".");
+            return;
+        }
+        for(int col = 0; col<chess[0].length; col++){
+            if(isSafeToPlace(chess, row, col)){
+                chess[row][col] = 1;
+                printNQueens(chess, qsf + row+"-"+col+", ", row+1);
+                chess[row][col] = 0;
+            }
+        }
+    }
+}
