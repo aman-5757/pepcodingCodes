b230f27c7945e7303024fd4ed4d0b859225fc32e
Aman Srivastava <aman4863@gmail.com>
2022-02-11 02:23:06 +0530

    added

--- DSA December/07. OOPs/Nodes/Main.java
+++ DSA December/07. OOPs/Nodes/Main.java
@@ -11,41 +11,49 @@ public class Main{
 
         }
 
-        student(String n){
-            name = n;
+        student(String name){
+            this.name = name;
         }
 
-        student(String n, int ID){
-            name = n;
-            id = ID;
+        student(String name, int id){
+            this.name = name;
+            this.id = id;
         }
 
-        student(String n, int ID, int bat){
-            name = n;
-            id = ID;
-            batch = bat;
+        student(String name, int id, int batch){
+            this.name = name;
+            this.id = id;
+            this.batch = batch;
         }
 
 
     }
 
     public static void main(String [] args){
-        student saksham = new student("Saksham", 7, 4);
-        student karan = new student("Karan", 11, 4);
-        student surjeet = new student("Surjeet", 6, 4);
-        student cr = surjeet;
-       
-        System.out.println("Name "  + cr.name);
-        System.out.println("ID "  + cr.id);
-        System.out.println("Batch "  + cr.batch);
-
-        // System.out.println("Name "  + karan.name);
-        // System.out.println("ID "  + karan.id);
-        // System.out.println("Batch "  + karan.batch);
+        // student aman = new student("Aman" , 5, 1);
+        student aman = new student("Aman" );
+        System.out.println("Name " + aman.name);
         
-        // System.out.println("Name "  + surjeet.name);
-        // System.out.println("ID "  + surjeet.id);
-        // System.out.println("Batch "  + surjeet.batch);
-
     }
+
+
+    // public static void main(String [] args){
+    //     student saksham = new student("Saksham", 7, 4);
+    //     student karan = new student("Karan", 11, 4);
+    //     student surjeet = new student("Surjeet", 6, 4);
+    //     student cr = surjeet;
+       
+    //     System.out.println("Name "  + cr.name);
+    //     System.out.println("ID "  + cr.id);
+    //     System.out.println("Batch "  + cr.batch);
+
+    //     // System.out.println("Name "  + karan.name);
+    //     // System.out.println("ID "  + karan.id);
+    //     // System.out.println("Batch "  + karan.batch);
+        
+    //     // System.out.println("Name "  + surjeet.name);
+    //     // System.out.println("ID "  + surjeet.id);
+    //     // System.out.println("Batch "  + surjeet.batch);
+
+    // }
 }
