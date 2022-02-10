ca0cbc1375388ae9a025ae2c0c6490db6a05ce8f
Aman Srivastava <aman4863@gmail.com>
2022-02-10 21:35:43 +0530

    added

--- DSA December/06. Recursion/backtracking/knightsTour/Main.java
+++ DSA December/06. Recursion/backtracking/knightsTour/Main.java
@@ -37,10 +37,43 @@ public class Main {
             int j = c + dir[d][1];
             
             printKnightsTour(chess, i, j, upcomingMove+1);
+            printKnightsTour_M2(chess, i, j, upcomingMove+1);
         }
         //unmark
         chess[r][c] = 0;
     }
+
+
+     public static void printKnightsTour_M2(int[][] chess, int r, int c, int upcomingMove) {
+        //out of range
+        if(r >= chess.length || c >= chess.length || r < 0 || c < 0 || chess[r][c] != 0 ){
+            return;
+        }
+
+        //destination
+        if(upcomingMove == chess.length * chess[0].length){
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+        //mark
+        chess[r][c] = upcomingMove;
+        //calls
+        printKnightsTour_M2(chess, r-2, c + 1, upcomingMove+1 );
+        printKnightsTour_M2(chess, r-1, c + 2, upcomingMove+1 );
+        printKnightsTour_M2(chess, r+1, c + 2, upcomingMove+1 );
+        printKnightsTour_M2(chess, r+2, c + 1, upcomingMove+1 );
+        printKnightsTour_M2(chess, r+2, c - 1, upcomingMove+1 );
+        printKnightsTour_M2(chess, r+1, c - 2, upcomingMove+1 );
+        printKnightsTour_M2(chess, r-1, c - 2, upcomingMove+1 );
+        printKnightsTour_M2(chess, r-2, c - 1, upcomingMove+1 );
+
+        //unmark
+        chess[r][c] = 0;
+    }
+
+
 
     public static void displayBoard(int[][] chess){
         for(int i = 0; i < chess.length; i++){
