63afd5e045e13065f95acf8f7c46cc58672db13f
Aman Srivastava <aman4863@gmail.com>
2022-02-12 21:23:34 +0530

    added

--- DSA December/08. LinkedList/linkedListCreation/Main.java
+++ DSA December/08. LinkedList/linkedListCreation/Main.java
@@ -35,6 +35,21 @@ public class Main {
       size++;
     }
 
+    public void addFirst(int val) {
+      Node temp = new Node();
+      temp.data = val;
+
+      if(size == 0){
+        head  = temp;
+        tail = temp;
+      }
+      else{
+        temp.next = head;
+        head = temp;
+      }
+      size++;
+    }
+
     public int size(){
       return size;
     }
@@ -48,6 +63,7 @@ public class Main {
       }
       System.out.println();
     }
+
 
 
   }
