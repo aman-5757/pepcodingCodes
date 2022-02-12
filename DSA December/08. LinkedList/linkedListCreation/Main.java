feee4dab19c6726f6a01969c4cf027fff0cadc8a
Aman Srivastava <aman4863@gmail.com>
2022-02-12 22:42:39 +0530

    get add

--- DSA December/08. LinkedList/linkedListCreation/Main.java
+++ DSA December/08. LinkedList/linkedListCreation/Main.java
@@ -81,6 +81,46 @@ public class Main {
       size--;
     }
 
+    public int getFirst(){
+      if(size == 0){
+       System.out.println("List is empty");
+       return -1; 
+      }
+      else{
+        return head.data;
+      }
+    }
+
+    public int getLast(){
+      if(size == 0){
+        System.out.println("List is empty");
+        return -1; 
+      }
+      else{
+        return tail.data;
+      }
+    }
+
+    public int getAt(int idx){
+      if(idx == 0){
+        return getFirst();
+      }
+      else if(idx == size - 1){
+        return getLast();
+      }
+      else if(idx < 0 || idx >= size){
+        System.out.println("Invalid arguments");
+        return -1;
+      }
+      else{
+        Node temp = head;
+        for(int i = 0; i < idx; i++){
+          temp = temp.next;
+        }
+        return temp.data;
+      }
+    }
+
 
 
   }
