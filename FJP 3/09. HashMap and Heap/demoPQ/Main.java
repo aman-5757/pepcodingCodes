c553c2e3475213b31ea93c298d153f3d44e168e5
Aman Srivastava <aman4863@gmail.com>
2022-02-16 23:02:15 +0530

    added

---
+++ FJP 3/09. HashMap and Heap/demoPQ/Main.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class Main{
+    public static void main(String [] args){
+        int [] arr =  {1,21, 57, 92, 13, 43, 22, 21, 6};
+
+        //create
+        PriorityQueue<Integer> pq = new PriorityQueue<>();          //default behaviour -- min PQ
+        for(int ele : arr)
+            pq.add(ele);
+
+        while(pq.size() != 0){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+
+    }
+}
