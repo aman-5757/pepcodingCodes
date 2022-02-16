3197f73187ae37c1f8a20fefc8c43bf7c431d8fa
Aman Srivastava <aman4863@gmail.com>
2022-02-16 23:36:14 +0530

    added

---
+++ FJP 3/09. HashMap and Heap/kLargesEle/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int[] arr = new int[n];
+
+      for (int i = 0; i < n; i++) {
+         arr[i] = Integer.parseInt(br.readLine());
+      }
+
+      int k = Integer.parseInt(br.readLine());
+      
+      PriorityQueue<Integer> pq = new PriorityQueue<>();    //min heap
+      for(int i = 0 ; i < n; i++){
+          if(i < k){
+              pq.add(arr[i]);
+          }
+          else{
+              if(pq.peek() < arr[i]){
+                  pq.remove();
+                  pq.add(arr[i]);
+              }
+          }
+      }
+      
+      while(pq.size() != 0){
+          System.out.println(pq.remove());
+      }
+      
+      
+    }
+
+}
