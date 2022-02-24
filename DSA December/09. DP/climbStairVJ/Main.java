922b29d0eb0a5edc36fd635fbb67799008516bee
Aman Srivastava <aman4863@gmail.com>
2022-02-24 23:13:01 +0530

    added

--- DSA December/09. DP/climbStairVJ/Main.java
+++ DSA December/09. DP/climbStairVJ/Main.java
@@ -32,6 +32,26 @@ public class Main {
     }
 
 
+    public static int climbStairVJTab(int SRC, int dest, int [] arr, int [] dp){
+
+        for(int src = dp.length-1; src>= 0; src-- ){
+            if(src == dest){
+                dp[src] = 1;
+                continue;
+            }
+            
+
+            int count = 0;
+            for(int jump = 1; jump <= arr[src] && jump + src <= dest; jump++){
+                count += dp[src+jump];
+            }
+            dp[src] = count;
+        }
+        return dp[SRC];
+        
+    }
+
+
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -43,7 +63,8 @@ public class Main {
         int [] dp = new int[n+1];
 
         // int ans = climbStairVJR(0, n, arr);
-        int ans = climbStairVJMemo(0, n, arr, dp);
+        // int ans = climbStairVJMemo(0, n, arr, dp);
+        int ans = climbStairVJTab(0, n, arr, dp);
         System.out.println(ans);
     }
 
