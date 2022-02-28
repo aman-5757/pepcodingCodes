19a7d3734e140b972f111f4f4227e25793db8520
Aman Srivastava <aman4863@gmail.com>
2022-02-28 23:00:49 +0530

    added

---
+++ FJP 3/10. Time and Space/insertionSort/Main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void insertionSort(int[] arr) {
+    int n = arr.length;
+    for(int x = 1; x < n; x++){
+      for(int i = x-1; i >= 0; i--){
+        if(isGreater(arr, i, i+1))// arr[i] > arr[i+1]){
+        {
+          swap(arr, i , i+1);
+        }
+        else{
+          break;
+        }
+      }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if jth element is greater than ith element
+  public static boolean isGreater(int[] arr, int j, int i) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    insertionSort(arr);
+    print(arr);
+  }
+
+}
