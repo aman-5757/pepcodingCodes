3658ea06ebdada6304d1f61912ca11f5c50d736e
Aman Srivastava <aman4863@gmail.com>
2022-03-15 22:20:00 +0530

    added

--- DSA December/10. Stack/infixConversion/Main.java
+++ DSA December/10. Stack/infixConversion/Main.java
@@ -28,22 +28,64 @@ public static void main(String[] args) throws Exception {
         char ch = exp.charAt(i);
         
         if(ch == '('){
-            
+            optr.push(ch);
         }
         else if((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ){
-            
+            postfix.push(ch+"");
+            prefix.push(ch+"");
         }
         else if(ch == ')'){
-            
+            while(optr.peek() != '('){
+                char operator = optr.pop();
+                String postV2 = postfix.pop();
+                String postV1 = postfix.pop();
+                
+                String calPost = postV1+postV2+operator;
+                postfix.push(calPost);
+                
+                String preV2 = prefix.pop();
+                String preV1 = prefix.pop();
+                
+                String calPre = operator+preV1+preV2;
+                prefix.push(calPre);
+                
+            }
+            optr.pop();
         }
         else if(isOptr(ch)){
-            
+            while(optr.size() != 0 && optr.peek() != '(' && precedence(optr.peek()) >= precedence(ch)){
+                char operator = optr.pop();
+                String postV2 = postfix.pop();
+                String postV1 = postfix.pop();
+                
+                String calPost = postV1+postV2+operator;
+                postfix.push(calPost);
+                
+                String preV2 = prefix.pop();
+                String preV1 = prefix.pop();
+                
+                String calPre = operator+preV1+preV2;
+                prefix.push(calPre);
+            }
+            optr.push(ch);
         }
     }
     
     while(optr.size() != 0){
-        
+        char operator = optr.pop();
+        String postV2 = postfix.pop();
+        String postV1 = postfix.pop();
+                
+        String calPost = postV1+postV2+operator;
+        postfix.push(calPost);
+                
+        String preV2 = prefix.pop();
+        String preV1 = prefix.pop();
+                
+        String calPre = operator+preV1+preV2;
+        prefix.push(calPre);
     }
+    
     
     
     
