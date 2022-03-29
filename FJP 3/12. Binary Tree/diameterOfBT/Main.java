e36bfd573ce4443760420fc15bac2b09aeea1aca
Aman Srivastava <aman4863@gmail.com>
2022-03-29 22:16:11 +0530

    ADDED

--- FJP 3/12. Binary Tree/diameterOfBT/Main.java
+++ FJP 3/12. Binary Tree/diameterOfBT/Main.java
@@ -4,6 +4,14 @@ class Solution {
     public int height(TreeNode root){
         return root == null ? -1 : Math.max(height(root.left) , height(root.right)) + 1;
     }
+	public int height(TreeNode root){
+        if( root == null )
+            return -1;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int myHeight = Math.max(lh,rh)+1;
+        return myHeight;
+    }	
     
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null)
