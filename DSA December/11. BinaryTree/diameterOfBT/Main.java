2ab015e357209f649847581d117685dcc1648f17
Aman Srivastava <aman4863@gmail.com>
2022-03-29 22:33:10 +0530

    added

--- DSA December/11. BinaryTree/diameterOfBT/Main.java
+++ DSA December/11. BinaryTree/diameterOfBT/Main.java
@@ -32,3 +32,42 @@ class Solution {
 
 
 // Approach 2
+
+class Solution {
+    public class pair{
+        int h;
+        int d;
+        
+        pair(){};
+        pair(int h, int d){
+            this.h = h;
+            this.d = d;
+        }
+    }
+    
+    public pair diameter(TreeNode node){
+        if(node == null){
+            return new pair(-1, 0);
+        }
+        
+        
+        pair la = diameter(node.left);
+        pair ra = diameter(node.right);
+        
+        pair my = new pair();
+        
+        my.h = Math.max(la.h , ra.h ) + 1;
+        int accrossNode = la.h + ra.h + 2; 
+        my.d = Math.max(accrossNode, Math.max(la.d, ra.d));
+        
+        return my;
+    }
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        pair ans = diameter(root);
+        return ans.d;
+    }
+}
+
+
+// Approach 3
--- FJP 3/12. Binary Tree/diameterOfBT/Main.java
+++ FJP 3/12. Binary Tree/diameterOfBT/Main.java
@@ -4,14 +4,15 @@ class Solution {
     public int height(TreeNode root){
         return root == null ? -1 : Math.max(height(root.left) , height(root.right)) + 1;
     }
-	public int height(TreeNode root){
+
+    public int height(TreeNode root){
         if( root == null )
             return -1;
         int lh = height(root.left);
         int rh = height(root.right);
         int myHeight = Math.max(lh,rh)+1;
         return myHeight;
-    }	
+    }
     
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null)
@@ -31,3 +32,42 @@ class Solution {
 
 
 // Approach 2
+
+class Solution {
+    public class pair{
+        int h;
+        int d;
+        
+        pair(){};
+        pair(int h, int d){
+            this.h = h;
+            this.d = d;
+        }
+    }
+    
+    public pair diameter(TreeNode node){
+        if(node == null){
+            return new pair(-1, 0);
+        }
+        
+        
+        pair la = diameter(node.left);
+        pair ra = diameter(node.right);
+        
+        pair my = new pair();
+        
+        my.h = Math.max(la.h , ra.h ) + 1;
+        int accrossNode = la.h + ra.h + 2; 
+        my.d = Math.max(accrossNode, Math.max(la.d, ra.d));
+        
+        return my;
+    }
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        pair ans = diameter(root);
+        return ans.d;
+    }
+}
+
+
+// Approach 3
