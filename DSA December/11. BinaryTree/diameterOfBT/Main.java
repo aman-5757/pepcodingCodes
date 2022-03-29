fdc4507ae83ec7bbd5950054662ee5843848e4f7
Aman Srivastava <aman4863@gmail.com>
2022-03-29 22:13:04 +0530

    added

---
+++ DSA December/11. BinaryTree/diameterOfBT/Main.java
@@ -0,0 +1,25 @@
+// Approach 1
+
+class Solution {
+    public int height(TreeNode root){
+        return root == null ? -1 : Math.max(height(root.left) , height(root.right)) + 1;
+    }
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        int ld = diameterOfBinaryTree(root.left);
+        int rd = diameterOfBinaryTree(root.right);
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int myDia = lh + rh + 2;
+        
+        return Math.max(myDia, Math.max(ld,rd));
+        
+    }
+}
+
+
+// Approach 2
