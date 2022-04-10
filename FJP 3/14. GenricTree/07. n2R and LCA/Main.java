a9662a6558198738916f7316dd8dc9a176ccc982
Aman Srivastava <aman4863@gmail.com>
2022-04-10 21:36:34 +0530

    added

---
+++ FJP 3/14. GenricTree/07. n2R and LCA/Main.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
+
+    return new ArrayList<>();
+  }
+
+  public static int lca(Node node, int d1, int d2) {
+    ArrayList<Integer> v1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> v2 = nodeToRootPath(node, d2);
+    
+    int i = v1.size()-1;
+    int j = v2.size()-1;
+    int LCA = -1;
+    while(i >= 0 && j >= 0){
+        if(v1.get(i)!= v2.get(j))
+            break;
+        LCA = v1.get(i);
+        i--;
+        j--;
+    }
+    return LCA;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int d1 = Integer.parseInt(br.readLine());
+    int d2 = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int lca = lca(root, d1, d2);
+    System.out.println(lca);
+    // display(root);
+  }
+
+}
