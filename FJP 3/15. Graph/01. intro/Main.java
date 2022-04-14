c7eb663ecd7374ccfde8d773935c212bd23a7873
Aman Srivastava <aman4863@gmail.com>
2022-04-14 22:28:28 +0530

    added

--- FJP 3/15. Graph/01. intro/Main.java
+++ FJP 3/15. Graph/01. intro/Main.java
@@ -13,13 +13,19 @@ public class Main{
     }
 
     public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w){
+        //because of undirected graph we are adding 2 times u-v & v-u
         graph[u].add(new Edge(u,v,w));
         graph[v].add(new Edge(v,u,w));
     }
 
     public static void construction(){
-        int N = 7;      //number of nodes
+        //N is number of vtx which will be given as i/p
+        int N = 7;     
+
+        //Array of ArrayList and AL will contain Edge also Edge will contain (u,v,w) 
         ArrayList<Edge>[] graph = new ArrayList[N];
+
+        //This is used because we are providing (allocating) memory to our each AL
         for(int i = 0; i < N; i++){
             graph[i] = new ArrayList<>();
         }
@@ -40,8 +46,11 @@ public class Main{
 
     public static void display(ArrayList<Edge>[] graph, int N){
         // N is number of vertex / Nodes
+
+        //Considering every vtx
         for(int n = 0; n < N; n++){
             System.out.print(n + " -> ");
+            //considering each edge of curr vertex
             for(Edge e : graph[n]){
                 int u = e.src;
                 int v = e.nbr;
