e5bc29bf584485d5aaf4652a8863ed2695b31c44
Aman Srivastava <aman4863@gmail.com>
2022-04-23 22:53:03 +0530

    added

---
+++ DSA December/14. Graph/12. PrimsAlgo/Main.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class pp{
+        //prims pair
+        int s;      //source
+        int p;          //parent
+        int cost;       //cost
+        
+        pp(){}
+        pp(int s, int p, int cost){
+            this.s = s;
+            this.p = p;
+            this.cost = cost;
+        }
+        
+    }
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+    public static void primsAlgo(ArrayList<Edge>[] graph){
+        int n = graph.length;
+        PriorityQueue<pp> pq = new PriorityQueue<>((a,b)->{
+            return a.cost-b.cost;
+        });
+        
+        boolean [] vis = new boolean[n];
+        
+        //seeding
+        pq.add(new pp(0, -1, 0));
+        
+        
+        while(pq.size() != 0){
+            //remove
+            pp rem = pq.remove();
+            
+            
+            //mark
+            
+            
+            //work
+            
+            
+            //add
+            
+            
+        }
+        
+        
+        
+    }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      //code starts here
+      primsAlgo();
+   }
+
+}
