6b13e211811e6e4795e8e9936e049e40df980640
Aman Srivastava <aman4863@gmail.com>
2022-05-10 22:59:32 +0530

    added

--- Level 2/01. recursion/lec001.java
+++ Level 2/01. recursion/lec001.java
@@ -62,9 +62,10 @@ public class lec001{
     }
 
 
+
     public static void mazePath(){
         int n = 3;
-        int m = 4;
+        int m = 3;
         String [] dirN = {"H", "V", "D"};
         int [][] dir = {{0,1}, {1,0}, {1,1}};
 
@@ -74,9 +75,41 @@ public class lec001{
     }
 
 
-    //======================================================================================
+    //===============================FLOOD FILL=======================================================
+    public static int floodFill_01(int sr, int sc, int dr, int dc, boolean [][] vis,  int [][] dir, String [] dirN, String asf){
+        if(sr == dr && sc == dc){
+            System.out.println(asf);
+            return 1;
+        }
+        vis[sr][sc] = true;
+        int count  = 0;
+        for(int d = 0; d < dir.length; d++){
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if(inRange(r,c,dr + 1, dc + 1) && !vis[r][c]){
+                count += floodFill_01(r,c, dr, dc,vis, dir, dirN, asf + dirN[d]);
+            }
+        }
+        vis[sr][sc] = false;
+        return count;
+
+    }
+
+    public static void floodFill(){
+        int n = 3;
+        int m = 3;
+        String [] dirN = {"A", "B", "C", "D", "E", "F", "G", "H"};
+        int [][] dir = {{-1, -1}, {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}};
+
+        boolean [][] vis = new boolean[n][m];
+        
+        int ans = floodFill_01(0,0,n-1,m-1,vis, dir, dirN, "");
+        System.out.println(ans);
+    }
 
     public static void main(String [] args){
-        mazePath();
+        // mazePath();
+        floodFill();
     }
 }
