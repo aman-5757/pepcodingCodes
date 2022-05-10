8d0442576a95c22cfea2bd423e39258ba972a04a
Aman Srivastava <aman4863@gmail.com>
2022-05-10 22:12:58 +0530

    added

--- Level 2/01. recursion/lec001.java
+++ Level 2/01. recursion/lec001.java
@@ -35,14 +35,41 @@ public class lec001{
 
     }
 
+    //With Jumps: Move
+    public static int mazePath_02(int sr, int sc, int dr, int dc, int [][] dir, String [] dirN, String asf){
+        if(sr == dr && sc == dc){
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count  = 0;
+        for(int d = 0; d < dir.length; d++){
+            
+            for(int move = 1; move <= Math.max(dr, dc); move++){
+
+                int r = sr + (dir[d][0] * move);
+                int c = sc + (dir[d][1] * move);
+
+                if(inRange(r,c,dr + 1, dc + 1)){
+                    count += mazePath_02(r,c, dr, dc, dir, dirN, asf + dirN[d]+move);
+                }
+            }
+
+            
+        }
+        return count;
+
+    }
+
 
     public static void mazePath(){
         int n = 3;
-        int m = 3;
+        int m = 4;
         String [] dirN = {"H", "V", "D"};
         int [][] dir = {{0,1}, {1,0}, {1,1}};
 
-        int ans = mazePath_01(0,0,n-1,m-1,dir, dirN, "");
+        // int ans = mazePath_01(0,0,n-1,m-1,dir, dirN, "");
+        int ans = mazePath_02(0,0,n-1,m-1,dir, dirN, "");
         System.out.println(ans);
     }
 
