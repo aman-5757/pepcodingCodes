e850429e8446534cb00e71fc88ec9797ab76e930
Aman Srivastava <aman4863@gmail.com>
2022-05-12 22:20:21 +0530

    added

---
+++ Level 2/01. recursion/lec002_RecursionTree.java
@@ -0,0 +1,25 @@
+public class lec002_RecursionTree{
+    public static int coinChangePermInf(int [] coins, int tar, String asf){
+        if(tar == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        int count = 0;
+        for(int i = 0; i<coins.length; i++){
+            if(tar - coins[i] >= 0){
+                count += coinChangePermInf(coins, tar-coins[i], asf + coins[i]);
+            }
+        }
+
+
+        return count;
+
+    }
+
+    public static void main(String [] args){
+        int [] coins = {2,3,5,7};
+        int tar = 10;
+        int ans = coinChangePermInf(coins, tar, "");
+        System.out.println("Count is -> " + ans);
+    }
+}
