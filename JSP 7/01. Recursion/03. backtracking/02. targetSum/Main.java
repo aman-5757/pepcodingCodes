16eaaabfd8d03bfd809cc6dcff3fcc3edd2d55f3
Aman Srivastava <aman4863@gmail.com>
2022-05-29 21:45:27 +0530

    added

---
+++ JSP 7/01. Recursion/03. backtracking/02. targetSum/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int tar = scn.nextInt();
+        printTargetSumSubsets(arr,0,"",0,tar);
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        if(sos > tar)
+            return;
+        if(idx == arr.length){
+            if(tar == sos){
+                System.out.println(set+".");
+            }
+            return;
+        }
+        //selected
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx]+", ", sos + arr[idx], tar);
+        
+        //not selected
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+    }
+
+}
