2aab3991a542ea17b90db3dcd4e887133f607270
Aman Srivastava <aman4863@gmail.com>
2022-06-03 23:15:47 +0530

    added

---
+++ JSP 7/03. LC 876 mid/Main.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){     
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
