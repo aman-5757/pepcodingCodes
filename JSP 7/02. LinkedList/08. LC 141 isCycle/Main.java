60ca7e7fab25738a871deee767ab79fbe5ab53d8
Aman Srivastava <aman4863@gmail.com>
2022-06-04 23:01:50 +0530

    added

---
+++ JSP 7/02. LinkedList/08. LC 141 isCycle/Main.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
