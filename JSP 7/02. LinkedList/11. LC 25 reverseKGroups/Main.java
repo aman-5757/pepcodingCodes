c133623bf6f17c0ffa8cad5b2bd85ad351ddda9d
Aman Srivastava <aman4863@gmail.com>
2022-06-05 22:08:52 +0530

    added

--- JSP 7/02. LinkedList/11. LC 25 reverseKGroups/Main.java
+++ JSP 7/02. LinkedList/11. LC 25 reverseKGroups/Main.java
@@ -25,6 +25,39 @@ class Solution {
     }
     
     public ListNode reverseKGroup(ListNode head, int k) {
+        if(k== 0 || head == null || head.next == null){
+            return head;
+        }
+        int len = length(head);
+        ListNode oh = null;
+        ListNode ot = null;
+        ListNode curr = head;
         
+        while(len >= k){
+            int tmp = k;
+            while(tmp-- > 0){
+                ListNode save = curr.next;
+                curr.next = null;
+                addFirst(curr);
+                curr = save;
+            }
+            if(oh == null){
+                oh = th;
+                ot = tt;
+            }
+            else{
+                ot.next = th;
+                ot = tt;
+            }
+            th = null;
+            tt = null;
+            
+            len -= k;
+        }
+        
+        ot.next = curr;
+        // head = oh;
+        // return head;
+        return oh;
     }
 }
