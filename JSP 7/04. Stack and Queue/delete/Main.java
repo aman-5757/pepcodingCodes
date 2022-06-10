098e14e19f6c2d108497b8adb5101ad44c6dde99
Aman Srivastava <aman4863@gmail.com>
2022-06-10 23:22:20 +0530

    added

---
+++ JSP 7/04. Stack and Queue/delete/Main.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+public class Main{
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<>();
+        while(true){
+            st.push(10);
+        }
+        
+    }
+}
