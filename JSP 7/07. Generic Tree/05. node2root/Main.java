7c956ea7ca73a81dc9d369c980838717dd960d51
Aman Srivastava <aman4863@gmail.com>
2022-06-18 22:01:31 +0530

    added

---
+++ JSP 7/07. Generic Tree/05. node2root/Main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+ public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    if(node.data == data){
+        ArrayList<Integer> base = new ArrayList<>();
+        base.add(data);
+        return base;
+    }
+    
+    for(Node c  : node.children){
+        ArrayList<Integer> temp = nodeToRootPath(c,data);
+        if(temp.size() != 0){
+            temp.add(node.data);
+            return temp;
+        }
+    }
+    
+    return new ArrayList<>();
+    
+ }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    ArrayList<Integer> path = nodeToRootPath(root, data);
+    System.out.println(path);
+    // display(root);
+  }
+
+}
