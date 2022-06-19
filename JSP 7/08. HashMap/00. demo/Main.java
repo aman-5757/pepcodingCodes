0f04fe2bd6539069f52e0e1d8c019bb98bb07201
Aman Srivastava <aman4863@gmail.com>
2022-06-19 23:17:19 +0530

    added

---
+++ JSP 7/08. HashMap/00. demo/Main.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+public class Main{
+    public static void main(String [] args){
+        //Create : Country <--> Population
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //add : put(K,V)
+
+        hm.put("India", 4200);
+        hm.put("China", 14200);
+        hm.put("USA", 84200);
+        hm.put("Bangladesh", 44200);
+        hm.put("Pak", 8200);
+
+        System.out.println(hm);
+        hm.put("USA", 2);
+
+        //Size()
+        int size = hm.size();
+        // System.out.println(size);
+
+        //print HM
+        System.out.println(hm);
+
+        //remove(K)
+        // hm.remove("India");
+        // System.out.println(hm);
+
+
+        // //get(K)
+        // int usa_ppl = hm.get("USA");
+        // System.out.println(usa_ppl);
+
+
+        // //keySet()
+        // for(String s : hm.keySet())
+        //     System.out.println(s);
+
+
+
+
+
+
+
+
+    }
+}
