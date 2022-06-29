95f5a5dc51564bac8f0ef1a2ecdb1a464b964325
Aman Srivastava <aman4863@gmail.com>
2022-06-29 23:40:31 +0530

    added

--- JSP 7/10. DP/05. goldmine/Main.java
+++ JSP 7/10. DP/05. goldmine/Main.java
@@ -2,6 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static void goldmine(int [][] arr, int [][] dp){
+        int n = arr.length;
+        int m = arr[0].length;
+        
+        for(int j = m-1; j>=0 ; j--){
+            for(int i = n-1; i >=0; i--){
+                if(j == m-1){
+                    dp[i][j] = arr[i][j];
+                }
+                    
+                else if(i == 0){
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j+1], dp[i+1][j+1]);
+                }
+                else if(i == n-1){
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j+1], dp[i-1][j+1]);
+                }
+                else{
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j+1], Math.max(dp[i+1][j+1], dp[i-1][j+1]));
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
@@ -16,6 +39,15 @@ public class Main {
         }
         
         //write your code here
+        int [][] dp = new int [n][m];
+        goldmine(arr, dp);
+        
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            max = Math.max(max, dp[i][0]);
+        }
+        System.out.println(max);
+        
     }
 
 }
