346ec3302b9bf921963eb2521067786aa2c0b397
Aman Srivastava <aman4863@gmail.com>
2022-06-29 22:53:03 +0530

    added

---
+++ JSP 7/10. DP/05. goldmine/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        
+        int [][] arr = new int[n][m];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        //write your code here
+    }
+
+}
